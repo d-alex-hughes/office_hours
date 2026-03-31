57adba0b3c4a04ed666d43f680655ef9e29b4ba5
D. Alex Hughes <dhughes@berkeley.edu>
2026-03-31 12:58:52 -0700

    downloads data

--- R/get_data.R
+++ R/get_data.R
@@ -1,7 +1,6 @@
 get_data <- function() { 
+  d <- data.table::fread(input = "https://people.ischool.berkeley.edu/~d.alex.hughes/data/hill_kousser_analysisFile.csv")
+  d <- janitor::clean_names(d)
   
-  data.table::fwrite(d, file = "./data/raw/data.csv")
+  data.table::fwrite(d, file = here::here("data/raw/data.csv"))
 }
-
-
-get_data()
